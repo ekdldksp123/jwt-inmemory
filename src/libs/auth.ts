1a4e69fe6be055e6df6b842fd15fffca199d3a12
ekdldksp123 <ekdldksp123@naver.com>
2022-05-19 08:40:05 +0900

    :recycle::heavy_plus_sign: refactor code and add mock server

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -1,16 +1,25 @@
 import Cookies from "universal-cookie"
 
 const cookies = new Cookies()
-export interface Tokens {
-    access_token?: string,
-    refresh_token?: string
+export interface AuthResponse {
+    result: Tokens
 }
 
+export interface Tokens {
+    accessToken: string,
+    refreshToken: string
+}
+
+const dummyToken =`{
+    "result": {
+      "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NTI5MTY0NTAsInRva2VuVHlwZSI6MSwiZXhwIjoxNjUyOTE4MjUwLCJpc3MiOiJodHRwczovL3Rlc3Qtand0LmNvbSJ9.Ej32gCE0MR2tHmvxFQ8ZRk8T8Wf4LlN9iU2_Q5c9r5g",
+      "refreshToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NTI5MTY0NTAsInRva2VuVHlwZSI6MTAxLCJ1dWlkIjoiY2JkNzE3MTgtYzc4OS00Njg5LTkwZjYtZmU5OGM2OTU1MmMzIiwiZXhwIjoxNjUyOTIwMDUwLCJpc3MiOiJodHRwczovL3Rlc3Qtand0LmNvbSJ9.mxRplbJHACxN4euo3udXLufCFM5N_Wvn9p3tWvtWlUY"
+    }
+}`
+
 const getTokens = () => {
-    return {
-        access_token: 'jx84e3kjew1njej3al2q9w',
-        refresh_token: 'g2rjfd7452bjfgn;a&*(jkehj',
-    } as Tokens
+    const authToken = JSON.parse(dummyToken) as AuthResponse
+    return authToken.result
 }
 
 export const login = (id:string, password:string) => {
@@ -24,12 +33,12 @@ export const login = (id:string, password:string) => {
 }
 
 const setTokensToCookie = (token: Tokens) => {
-    cookies.set('access_token', token.access_token, { 
+    cookies.set('access_token', token.accessToken, { 
         sameSite: 'strict',
         path: '/',
         expires: new Date(new Date().getTime() + 5 * 60000) 
     })
-    cookies.set('refresh_token', token.refresh_token, { 
+    cookies.set('refresh_token', token.refreshToken, { 
         sameSite: 'strict',
         path: '/',
         expires: new Date(new Date().getTime() + 5 * 60000) 
