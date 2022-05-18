29aa9436868e3c04573c991314b3629db859b9c8
ekdldksp123 <ekdldksp123@naver.com>
2022-05-18 15:39:40 +0900

    :sparkles: implementation of storing and deleting jwt tokens in cookies and add immer dependencies

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -1,16 +1,42 @@
+import Cookies from "universal-cookie"
+
+const cookies = new Cookies()
 export interface Tokens {
     access_token?: string,
     refresh_token?: string
 }
 
+const getTokens = () => {
+    return {
+        access_token: 'jx84e3kjew1njej3al2q9w',
+        refresh_token: 'g2rjfd7452bjfgn;a&*(jkehj',
+    } as Tokens
+}
+
 export const login = (id:string, password:string) => {
     if(id === 'rest' && password === 'test') {
-        return {
-            access_token: 'jx84e3kjew1njej3al2q9w',
-            refresh_token: 'g2rjfd7452bjfgn;a&*(jkehj',
-        } as Tokens
+        const jwtTokens = getTokens()
+        setTokensToCookie(jwtTokens)
+        return jwtTokens
     } else {
         return undefined
     }
 }
 
+const setTokensToCookie = (token: Tokens) => {
+    cookies.set('access_token', token.access_token, { sameSite: 'strict' })
+    cookies.set('refresh_token', token.refresh_token, { sameSite: 'strict' })
+}
+
+export const deleteAccessTokenFromCookie = () => {
+    cookies.remove('access_token')
+}
+
+export const deleteRefreshTokenFromCookie = () => {
+    cookies.remove('refresh_token')
+}
+
+export const deleteJwtTokensFromCookie = () => {
+    cookies.remove('access_token')
+    cookies.remove('refresh_token')
+}
--- src/pages/Hello.tsx
+++ src/pages/Hello.tsx
@@ -1,16 +1,22 @@
 import { Navigate } from "react-router-dom"
 import { Container } from "../components/layout/default"
+import { deleteJwtTokensFromCookie } from "../libs/auth"
 import { useStore } from "../libs/store"
 
 const Hello: React.FC = () => {
 
     const state = useStore(state => state)
-    
+
+    const handleLogout = () => {
+        state.deleteToken()
+        deleteJwtTokensFromCookie()
+    }
+
     if(state.authenticated) {
         return (
             <Container>
                 <h1>Hello</h1>
-                <button onClick={() => state.deleteToken()}>Logout</button>
+                <button onClick={() => handleLogout()}>Logout</button>
             </Container>
         )
     } else {
